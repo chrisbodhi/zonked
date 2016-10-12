b60d05802161d037ae52d5e304c3f3ff46d9a5b4
Chris Boette <cboette@gmail.com>
2016-10-11 22:52:32 -0500

    Add time and author fields. Change slug for URL to slash-api.

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+import "time"
 import "github.com/gin-gonic/gin"
 
 func main() {
     r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
+
+    r.GET("/api", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
+            "author": "cb",
+            "time": time.Now(),
         })
     })
     r.Run() // listen and server on 0.0.0.0:8080
